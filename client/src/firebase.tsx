979c7945c18c4796c9a8ef8ba596922d10b15e68
JosseMontano <josemariazambranamontano123@gmail.com>
2023-01-19 15:12:51 -0400

    it was added env in the front

--- client/src/firebase.tsx
+++ client/src/firebase.tsx
@@ -1,26 +1,14 @@
-// Import the functions you need from the SDKs you need
-
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-
-// TODO: Add SDKs for Firebase products that you want to use
-
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
+import config from "./config";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAps-i4jcF9CRPq1NUVN4Vs95e1oXaYA3I",
-
-  authDomain: "gomovies-2f287.firebaseapp.com",
-
-  projectId: "gomovies-2f287",
-
-  storageBucket: "gomovies-2f287.appspot.com",
-
-  messagingSenderId: "973492262571",
-
-  appId: "1:973492262571:web:7a0f29c44a47ff0c3649ee",
+  apiKey: config.apikey,
+  authDomain: config.authDomain,
+  projectId: config.projectId,
+  storageBucket: config.storageBucket,
+  messagingSenderId: config.messagingSenderId,
+  appId: config.appId,
 };
 
 // Initialize Firebase
