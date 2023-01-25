f4bfbe323e1032795582e6ccf17772f83332d8bf
JosseMontano <josemariazambranamontano123@gmail.com>
2023-01-25 19:12:10 -0400

    validation un chapter

--- server/controllers/chapter.go
+++ server/controllers/chapter.go
@@ -1,10 +1,13 @@
 package controllers
 
 import (
+	"fmt"
 	"strconv"
+	"strings"
 
 	"github.com/JosseMontano/go-series/database"
 	"github.com/JosseMontano/go-series/models"
+	"github.com/go-playground/validator/v10"
 	"github.com/gofiber/fiber/v2"
 	"github.com/google/uuid"
 )
@@ -16,6 +19,22 @@ func AllChapterBySerie(c *fiber.Ctx) error {
 	database.DB.Where("id", id).Find(&chapter)
 	return c.JSON(chapter)
 } */
+
+var ValidateChapter = validator.New()
+
+func ValidateCreateChapter(chapter models.Chapter) string {
+	err := ValidateChapter.Struct(chapter)
+	errormsg := ""
+	fmt.Print(err)
+	if err != nil {
+		for _, err := range err.(validator.ValidationErrors) {
+			field := strings.Replace(err.StructNamespace(), "Chapter.", "", -1)
+			errormsg = errormsg + field + " " + err.Tag() + ","
+		}
+		errormsg = strings.TrimSuffix(errormsg, ",")
+	}
+	return errormsg
+}
 
 func CreateChapters(c *fiber.Ctx) error {
 	var data map[string]string
@@ -34,6 +53,13 @@ func CreateChapters(c *fiber.Ctx) error {
 		Description: data["description"],
 		Duration:    duration,
 		Url:         data["url"],
+	}
+
+	if err := ValidateCreateChapter(chapter); err != "" {
+		c.Status(fiber.StatusBadRequest)
+		return c.JSON(fiber.Map{
+			"message": err,
+		})
 	}
 
 	database.DB.Create(&chapter)
--- server/models/chapter.go
+++ server/models/chapter.go
@@ -2,8 +2,14 @@ package models
 
 type Chapter struct {
 	Id          string  `json:"id" gorm:"primaryKey"`
-	Title       string  `json:"title"`
-	Description string  `json:"description"`
-	Duration    float64 `json:"duration"`
-	Url         string  `json:"url"`
+	Title       string  `json:"title" validate:"required"`
+	Description string  `json:"description" validate:"required"`
+	Duration    float64 `json:"duration" validate:"required"`
+	Url         string  `json:"url" validate:"required"`
+}
+
+type ValidateChapter struct {
+	FailedFiled string
+	Tag         string
+	Value       string
 }
