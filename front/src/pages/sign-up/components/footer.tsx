a8e07dba457d9fb2a91334a007fcd623143d4d4f
JosseMontano <josemariazambranamontano123@gmail.com>
2023-01-31 14:02:22 -0400

    p-welcome:styled to jsx

--- front/src/pages/sign-up/components/footer.tsx
+++ front/src/pages/sign-up/components/footer.tsx
@@ -1,21 +1,21 @@
-import styled from "styled-components";
 import { colorPrimary } from "@/styles/colors";
 type FooterProps = {
   signIn: () => void;
 };
 
-const Container = styled.p<{ colorPrimary: string }>`
-  span {
-    cursor: pointer;
-    color: ${(p) => p.colorPrimary};
-  }
-`;
-
 export const Footer = ({ signIn }: FooterProps) => (
-  <Container colorPrimary={colorPrimary}>
+  <div>
     Already have an account?{" "}
     <span data-testid="btn" onClick={signIn}>
       Sign In
     </span>
-  </Container>
+    <style jsx>
+      {`
+        span {
+          cursor: pointer;
+          color: ${colorPrimary};
+        }
+      `}
+    </style>
+  </div>
 );
