4fbbe92274729ab0710f08f532d53e98f165c0e2
JosseMontano <josemariazambranamontano123@gmail.com>
2023-01-31 14:08:18 -0400

    p-start:styled to jsx

--- front/src/pages/index.tsx
+++ front/src/pages/index.tsx
@@ -1,14 +1,7 @@
 import { useRouter } from "next/router";
-import styled from "styled-components";
 import { Icon } from "./index/components/icon";
 import { Loader } from "./index/components/loader";
 import ImgReact from "@/assets/react.png";
-
-const Container = styled.div`
-  display: grid;
-  height: 100vh;
-  place-content: center;
-`;
 
 export default function Home() {
   const router = useRouter();
@@ -17,11 +10,20 @@ export default function Home() {
     router.push("/welcome");
   }, 3000);
   return (
-    <Container>
+    <div className="container">
       <Loader />
       <div>
         <Icon IconImg={ImgReact} />
       </div>
-    </Container>
+      <style jsx>
+        {`
+          .container {
+            display: grid;
+            height: 100vh;
+            place-content: center;
+          }
+        `}
+      </style>
+    </div>
   );
 }
--- front/src/pages/index/components/icon.tsx
+++ front/src/pages/index/components/icon.tsx
@@ -1,24 +1,25 @@
-"use client"
+"use client";
 
 import { StaticImageData } from "next/image";
-import styled from "styled-components";
 import Image from "next/image";
 
 type IconProps = {
   IconImg: StaticImageData;
 };
 
-const Container = styled.div`
-  img {
-    background-color: transparent;
-    width: 150px;
-    height: 150px;
-    justify-self: center;
-  }
-`;
 
 export const Icon = ({ IconImg }: IconProps) => (
-  <Container>
-    <Image src={IconImg} alt="icon" />
-  </Container>
+  <div className="container">
+    <Image className="img_start" src={IconImg} alt="icon" />
+    <style jsx global>
+      {`
+        .img_start {
+          background-color: transparent;
+          width: 150px;
+          height: 150px;
+          justify-self: center;
+        }
+      `}
+    </style>
+  </div>
 );
